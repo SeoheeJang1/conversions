a59f4fddca477444472402759e2bf1e859c3079b
seoheejang <seohee.jang@postgrad.manchester.ac.uk>
2021-10-23 11:50:03 +0100

    add help text for script

--- Temperature.m
+++ Temperature.m
@@ -1,4 +1,6 @@
 % Save degree symbol as a variable
+%TEMPERATURE_CONVERSIONS
+% Convert key temperature values between Fahrenheit, Kelvin, and Celsius
 deg = char(176);
 
 disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(100)), deg, 'F.'])
