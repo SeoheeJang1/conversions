97a75f78b250161f6111e555f8d2350b01bb358f
seoheejang <seohee.jang@postgrad.manchester.ac.uk>
2021-10-23 23:41:55 +0100

    Last file

--- Temperature.m
+++ Temperature.m
@@ -2,7 +2,10 @@
 %TEMPERATURE_CONVERSIONS
 % Convert key temperature values between Fahrenheit, Kelvin, and Celsius
 deg = char(176);
-Typepooppo
+degC=linspace(0,100,101);
+plot(degC,celsius_to_fahrenheit(degC))
+xlabel('Celsius')
+ylabel('Fahrenheit')
 disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(100)), deg, 'F.'])
 fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
 fprintf('Absolute zero is 0K, which is %g%sC.\n',kelvin_to_celsius(0),deg)
