e55c4c488fd57d212b85ab02eea427006cd002a8
seoheejang <seohee.jang@postgrad.manchester.ac.uk>
2021-10-23 23:21:54 +0100

    Revert "file"
    This reverts commit 9ef3f2e7a4648562bd1267b00c7eaa4299e67d35.

--- Temperature.m
+++ Temperature.m
@@ -2,7 +2,7 @@
 %TEMPERATURE_CONVERSIONS
 % Convert key temperature values between Fahrenheit, Kelvin, and Celsius
 deg = char(176);
-
+Typepooppo
 disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(100)), deg, 'F.'])
 fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
 fprintf('Absolute zero is 0K, which is %g%sC.\n',kelvin_to_celsius(0),deg)
